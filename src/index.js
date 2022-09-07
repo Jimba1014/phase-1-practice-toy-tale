7ee35a62488f4da35126bd9117a75613e7429eac
Jim <jimba.ed@outlook.com>
2022-09-07 09:38:00 -0400

    commit tree 2175c8ce6e26a57d17db2d278da54cdb8ee2df65

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 let addToy = false;
+//Below is the code that I added
+function fetchToys(){
+  return fetch('')
+  .then((resp) => resp.json())
+  .then((json) => renderToy(json));
+}
 
+doucment.addEventListener("DOMContentLoaded", () =>{
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    renderyou(e.target.newToyLike)
+  })
+}
+function renderToy(toy){
+  const newToy = document.querySelector("toy-collection");
+  toy.forEach(toy =>{
+    const h2 = document.createElement("h2");
+    h2.innerHTML = toy.name;
+    newToy.appendChild(h2);
+  })
+}
+
+function renderToyCard()
+
+//above is the code that I added
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  //added preventDefault so the page would not reload 
+  //on the create toy button
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -13,3 +40,5 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
